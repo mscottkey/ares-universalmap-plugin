d791d4b23391d7e2fecea87fc903f7b7ee0debda
mscottkey <mscottkey@gmail.com>
2025-05-01 06:24:26 +0000

    Fixed an Ohm sort_by issue with listing maps

--- plugin/commands/map_list_cmd.rb
+++ plugin/commands/map_list_cmd.rb
@@ -4,10 +4,14 @@ module AresMUSH
       include CommandHandler
 
       def handle
-        maps = UniversalMapGrid.all.sort_by(:created_at, :desc)
+        maps = UniversalMapGrid.all.to_a.sort_by { |m| m.created_at }.reverse
+        if maps.empty?
+          client.emit "No maps found."
+          return
+        end
         template = MapListTemplate.new(maps)
         client.emit template.render
-      end
+      end      
     end
   end
 end
