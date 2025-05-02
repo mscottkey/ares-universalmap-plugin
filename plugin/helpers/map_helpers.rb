172ae1564e0e5a40a4db9c4d611c43ba4fcae89f
mscottkey <mscottkey@gmail.com>
2025-05-02 06:45:25 +0000

    Switched to ,find to avoid redis index issus.

--- plugin/helpers/map_helpers.rb
+++ plugin/helpers/map_helpers.rb
@@ -4,7 +4,6 @@ module AresMUSH
 
       def self.format_object(object_key, web: false)
         config = Global.read_config("universal_map_objects", object_key)
-
         return object_key.titlecase if config.nil?
 
         label = config["label"] || object_key.titlecase
@@ -18,17 +17,18 @@ module AresMUSH
       end
 
       def self.safe_tokens(map)
-        map.tokens.to_a
-      rescue Ohm::IndexNotFound
-        []
-      end
-  
-      def self.safe_objects(map)
-        map.objects.to_a
-      rescue Ohm::IndexNotFound
+        UniversalMapToken.find(map: map).to_a
+      rescue => e
+        Global.logger.warn "🔧 Failed to fetch tokens: \#{e}"
         []
       end
 
+      def self.safe_objects(map)
+        UniversalMapObject.find(map: map).to_a
+      rescue => e
+        Global.logger.warn "🔧 Failed to fetch objects: \#{e}"
+        []
+      end
     end
   end
 end
